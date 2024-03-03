c3a95256b9f8b8a7cf33e3706d82cb82894768e0
Cooper Gregory <cooper.c.gregory@gmail.com>
2024-03-03 22:11:52 +1100

    Adding colour change

---
+++ Assets/Scripts/StateInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Element")]
+public class StateInfo : ScriptableObject
+{
+    public int iD;
+    public string element;
+}
